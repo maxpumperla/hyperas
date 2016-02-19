d60e3d8fbb9abe9a31631d094f61f90ce7f9f2b6
Max Pumperla <max.pumperla@googlemail.com>
2016-02-19 17:44:24 +0100

    very simple first example

---
+++ examples/simple.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+from hyperopt import Trials, STATUS_OK, tpe
+from hyperas import optim
+from hyperas.distributions import choice, uniform
+
+
+def keras_model():
+    from keras.datasets import mnist
+    from keras.models import Sequential
+    from keras.layers.core import Dense, Dropout, Activation
+    from keras.optimizers import RMSprop
+    from keras.utils import np_utils
+
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    X_train = X_train.reshape(60000, 784)
+    X_test = X_test.reshape(10000, 784)
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+    nb_classes = 10
+
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+    model = Sequential()
+    model.add(Dense(512, input_shape=(784,)))
+    model.add(Activation('relu'))
+    model.add(Dropout({{uniform(0, 1)}}))
+    model.add(Dense({{choice([256, 512, 1024])}}))
+    model.add(Activation('relu'))
+    model.add(Dropout({{uniform(0, 1)}}))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+
+    rms = RMSprop()
+    model.compile(loss='categorical_crossentropy', optimizer=rms)
+
+    model.fit(X_train, Y_train,
+              batch_size={{choice([64, 128])}},
+              nb_epoch=1,
+              show_accuracy=True,
+              verbose=2,
+              validation_data=(X_test, Y_test))
+    score = model.evaluate(X_test, Y_test,
+                           show_accuracy=True, verbose=0)
+    print('Test accuracy:', score[1])
+    return {'loss': -score[1], 'status': STATUS_OK}
+
+if __name__ == '__main__':
+    best_run = optim.minimize(keras_model,
+                              algo=tpe.suggest,
+                              max_evals=10,
+                              trials=Trials())
+    print(best_run)
