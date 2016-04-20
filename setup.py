5bf447806108b5e1b896263c6e6c3388b36e49e2
Max Pumperla <max.pumperla@googlemail.com>
2016-04-20 13:55:35 +0200

    prepare release 0.2

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='hyperas',
-      version='0.1.2',
+      version='0.2',
       description='Simple wrapper for hyperopt to do convenient hyperparameter optimization for Keras models',
       url='http://github.com/maxpumperla/hyperas',
-      download_url='https://github.com/maxpumperla/hyperas/tarball/0.1.2',
+      download_url='https://github.com/maxpumperla/hyperas/tarball/0.2',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
       install_requires=['keras', 'hyperopt'],
