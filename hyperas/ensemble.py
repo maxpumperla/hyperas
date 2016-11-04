3142eaac38099003552c2cca26e82e24a304983d
Emmanuel Kahembwe <MannyKayy@users.noreply.github.com>
2016-11-04 19:59:19 +0000

    Enable soft voting
    Enable soft voting and directly pass through the number of classes

--- hyperas/ensemble.py
+++ hyperas/ensemble.py
@@ -4,7 +4,7 @@ from keras.models import model_from_yaml
 
 class VotingModel(object):
 
-    def __init__(self, model_list, voting='hard', weights=None):
+    def __init__(self, model_list, voting='hard', weights=None, nb_classes=None):
         """(Weighted) majority vote model for a given list of Keras models.
 
         Parameters
@@ -13,6 +13,7 @@ class VotingModel(object):
         voting: Choose 'hard' for straight-up majority vote of highest model probilities or 'soft'
             for a weighted majority vote. In the latter, a weight vector has to be specified.
         weights: Weight vector (numpy array) used for soft majority vote.
+        nb_classes: Number of classes being predicted.
 
         Returns
         -------
@@ -21,6 +22,7 @@ class VotingModel(object):
         self.model_list = model_list
         self.voting = voting
         self.weights = weights
+        self.nb_classes = nb_classes
 
         if voting not in ['hard', 'soft']:
             raise 'Voting has to be either hard or soft'
@@ -33,7 +35,6 @@ class VotingModel(object):
     def predict(self, X, batch_size=128, verbose=0):
         predictions = list(map(lambda model: model.predict(X, batch_size, verbose), self.model_list))
         nb_preds = len(X)
-        nb_classes = len(predictions[0])
 
         if self.voting == 'hard':
             for i, pred in enumerate(predictions):
@@ -43,8 +44,8 @@ class VotingModel(object):
             votes = np.asarray(list(map(lambda arr: max(set(arr)), argmax_list)))
         if self.voting == 'soft':
             for i, pred in enumerate(predictions):
-                pred = map(lambda probas: probas * self.weights[i], pred)
-                predictions[i] = np.asarray(pred).reshape(nb_preds, nb_classes, 1)
+                pred = list(map(lambda probas: probas * self.weights[i], pred))
+                predictions[i] = np.asarray(pred).reshape(nb_preds, self.nb_classes, 1)
             weighted_preds = np.concatenate(predictions, axis=2)
             weighted_avg = np.mean(weighted_preds, axis=2)
             votes = np.argmax(weighted_avg, axis=1)
