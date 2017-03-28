13ca74da7d04dfff50838bd9310141924d448410
Ambros94 <luca.ambrosini@supsi.ch>
2017-03-28 19:44:49 +0200

    Updated readme examples for keras 2.0, solving #67 #56 Put the readme example in examples folder

--- examples/mnist_readme.py
+++ examples/mnist_readme.py
@@ -1,69 +1,3 @@
-# Hyperas [![Build Status](https://travis-ci.org/maxpumperla/hyperas.svg?branch=master)](https://travis-ci.org/maxpumperla/hyperas)  [![PyPI version](https://badge.fury.io/py/hyperas.svg)](https://badge.fury.io/py/hyperas) 
-A very simple convenience wrapper around hyperopt for fast prototyping with keras models. Hyperas lets you use the power of hyperopt without having to learn the syntax of it. Instead, just define your keras model as you are used to, but use a simple template notation to define hyper-parameter ranges to tune.
-
-## Installation
-```{python}
-pip install hyperas
-```
-
-## Quick start
-Assume you have an existing keras model like the following.
-```{python}
-model = Sequential()
-model.add(Dense(512, input_shape=(784,)))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.2)
-model.add(Dense(10))
-model.add(Activation('softmax'))
-```
-To do hyper-parameter optimization on this model, just wrap the parameters you want to optimize into double curly brackets and choose a distribution over which to run the algorithm. In the above example, let's say we want to optimize for the best dropout probability in both dropout layers. Choosing a uniform distribution over the interval ```[0,1]```, this translates into the following definition.
-
-```{python}
-from hyperas.distributions import uniform
-
-model = Sequential()
-model.add(Dense(512, input_shape=(784,)))
-model.add(Activation('relu'))
-model.add(Dropout({{uniform(0, 1)}}))
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout({{uniform(0, 1)}}))
-model.add(Dense(10))
-model.add(Activation('softmax'))
-```
-
-After having trained the model, to optimize, we also have to define which evaluation metric of the model is important to us. For example, if we wish to optimize for accuracy, the following example does the trick:
-
-```{python}
-score = model.evaluate(X_test, Y_test, verbose=0)
-accuracy = score[1]
-return {'loss': -accuracy, 'status': STATUS_OK}
-```
-The last step is to actually run the optimization, which is done as follows:
-
-```{python}
-best_run = optim.minimize(model=model,
-                          data=data,
-                          algo=tpe.suggest,
-                          max_evals=10,
-                          trials=Trials())
-```
-In this example we use at most 10 evaluation runs and the TPE algorithm from hyperopt for optimization.
-
-## Complete example
-**Note:** It is important to wrap your data and model into functions as shown below, and then pass them as parameters to the minimizer. ```data()``` returns the data the ```model()``` needs. An extended version of the above example in one script reads as follows. This example shows many potential use cases of hyperas, including:
-- Varying dropout probabilities, sampling from a uniform distribution
-- Different layer output sizes
-- Different optimization algorithms to use
-- Varying choices of activation functions
-- Conditionally adding layers depending on a choice
-- Swapping whole sets of layers
-
-
-```{python}
 from __future__ import print_function
 
 from hyperopt import Trials, STATUS_OK, tpe
@@ -151,4 +85,3 @@ if __name__ == '__main__':
     print(best_model.evaluate(X_test, Y_test))
     print("Best performing model chosen hyper-parameters:")
     print(best_run)
-```
