ae3220093c2bbd914cc2c482196264c55d89fce7
AHoke <wzxywzl@163.com>
2017-05-03 14:58:58 +0800

    A new examples shows the usage of 'functions',and "import xx as xx" format.

---
+++ examples/use_intermediate_functions.py
@@ -0,0 +1,94 @@
+from __future__ import print_function
+from hyperopt import Trials, STATUS_OK, tpe
+from hyperas import optim
+from hyperas.distributions import choice, uniform
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import RMSprop
+
+from keras.datasets import mnist
+from keras.utils import np_utils
+
+import matplotlib.pyplot as plt
+def visualization_mnist(x_data,n=10):
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+        # display digit
+        ax = plt.subplot(1, n, i+1)
+        plt.imshow(x_data[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
+
+
+def data():
+    '''
+    Data providing function:
+
+    This function is separated from model() so that hyperopt
+    won't reload data for each evaluation run.
+    '''
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    X_train = X_train.reshape(60000, 784)
+    X_test = X_test.reshape(10000, 784)
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    
+    visualization_mnist(X_test)
+    
+    X_train /= 255
+    X_test /= 255
+    nb_classes = 10
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    return X_train, Y_train, X_test, Y_test
+
+
+def model(X_train, Y_train, X_test, Y_test):
+    '''
+    Model providing function:
+
+    Create Keras model with double curly brackets dropped-in as needed.
+    Return value has to be a valid python dictionary with two customary keys:
+        - loss: Specify a numeric evaluation metric to be minimized
+        - status: Just use STATUS_OK and see hyperopt documentation if not feasible
+    The last one is optional, though recommended, namely:
+        - model: specify the model just created so that we can later use it again.
+    '''
+    model = Sequential()
+    model.add(Dense(512, input_shape=(784,)))
+    model.add(Activation('relu'))
+    model.add(Dropout({{uniform(0, 1)}}))
+    model.add(Dense({{choice([256, 512, 1024])}}))
+    model.add(Activation('relu'))
+    model.add(Dropout({{uniform(0, 1)}}))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+
+    rms = RMSprop()
+    model.compile(loss='categorical_crossentropy', optimizer=rms, metrics=['accuracy'])
+
+    model.fit(X_train, Y_train,
+              batch_size={{choice([64, 128])}},
+              nb_epoch=1,
+              verbose=2,
+              validation_data=(X_test, Y_test))
+    score, acc = model.evaluate(X_test, Y_test, verbose=0)
+    print('Test accuracy:', acc)
+    return {'loss': -acc, 'status': STATUS_OK, 'model': model}
+
+if __name__ == '__main__':
+
+    X_train, Y_train, X_test, Y_test = data()
+    functions=[visualization_mnist]
+    best_run, best_model = optim.minimize(model=model,
+                                          data=data,
+                                          functions=functions,
+                                          algo=tpe.suggest,
+                                          max_evals=5,
+                                          trials=Trials())
+
+    print("Evalutation of best performing model:")
+    print(best_model.evaluate(X_test, Y_test))
