1ec98a092724b78ffb4654f3d31798c0879b2759
Philipp Kainz <kainz.p@gmail.com>
2017-06-06 13:51:53 +0200

    updated install requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup
 from setuptools import find_packages
 
+install_requirements = [
+    'keras',
+    'hyperopt',
+    'entrypoints',
+    'ipython<6.0',
+    'jupyter',
+    'nbformat',
+    'nbconvert'
+]
+
 setup(name='hyperas',
       version='0.3',
       description='Simple wrapper for hyperopt to do convenient hyperparameter optimization for Keras models',
@@ -8,7 +18,7 @@ setup(name='hyperas',
       download_url='https://github.com/maxpumperla/hyperas/tarball/0.3',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
-      install_requires=['keras', 'hyperopt', 'entrypoints', 'ipython', 'jupyter', 'nbformat', 'nbconvert'],
+      install_requires=install_requirements,
       license='MIT',
       packages=find_packages(),
       zip_safe=False)
