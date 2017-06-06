b5961d958552f02eb714710be3d8aa30b83cdb01
Philipp Kainz <kainz.p@gmail.com>
2017-06-06 14:04:07 +0200

    dependency update

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 from setuptools import setup
 from setuptools import find_packages
 
-install_requirements = [
-    'keras',
-    'hyperopt',
-    'entrypoints',
-    'ipython[all]<6.0',
-    'jupyter',
-    'nbformat',
-    'nbconvert'
-]
-
 setup(name='hyperas',
       version='0.3',
       description='Simple wrapper for hyperopt to do convenient hyperparameter optimization for Keras models',
@@ -18,7 +8,7 @@ setup(name='hyperas',
       download_url='https://github.com/maxpumperla/hyperas/tarball/0.3',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
-      install_requires=install_requirements,
+      install_requires=['keras', 'hyperopt', 'entrypoints', 'jupyter', 'nbformat', 'nbconvert'],
       license='MIT',
       packages=find_packages(),
       zip_safe=False)
