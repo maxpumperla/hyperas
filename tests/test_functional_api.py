0a8842486c370dea868fcd27f45792c75f35ef52
maxpumperla <max.pumperla@googlemail.com>
2018-06-13 10:01:56 +0200

    closes #139

---
+++ tests/test_functional_api.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+from hyperopt import Trials, STATUS_OK, tpe
+from hyperas import optim
+from hyperas.distributions import choice, uniform
+
+from keras.models import Model
+from keras.layers import Dense, Input
+from keras.optimizers import RMSprop
+
+from keras.datasets import mnist
+from keras.utils import np_utils
+from hyperopt import rand
+
+
+def data():
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    X_train = X_train.reshape(60000, 784)
+    X_test = X_test.reshape(10000, 784)
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+    nb_classes = 10
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    return X_train, Y_train, X_test, Y_test
+
+
+def model(X_train, Y_train, X_test, Y_test):
+    inputs = Input(shape=(784,))
+
+    x = Dense({{choice([20, 30, 40])}}, activation='relu')(inputs)
+    x = Dense(64, activation='relu')(x)
+    predictions = Dense(10, activation='softmax')(x)
+    model = Model(inputs=inputs, outputs=predictions)
+
+    rms = RMSprop()
+    model.compile(loss='categorical_crossentropy', optimizer=rms, metrics=['accuracy'])
+
+    model.fit(X_train, Y_train,
+              batch_size={{choice([64, 128])}},
+              nb_epoch=1,
+              verbose=2,
+              validation_data=(X_test, Y_test))
+    score, acc = model.evaluate(X_test, Y_test, verbose=0)
+    print('Test accuracy:', acc)
+    return {'loss': -acc, 'status': STATUS_OK, 'model': model}
+
+
+def test_functional_api():
+    X_train, Y_train, X_test, Y_test = data()
+    best_run, best_model = optim.minimize(model=model,
+                                          data=data,
+                                          algo=tpe.suggest,
+                                          max_evals=1,
+                                          trials=Trials(),
+                                          verbose=False)
