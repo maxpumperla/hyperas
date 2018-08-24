00b7342ab332d872e754050c5f333f93f650edf2
haramoz <arkamallick30@gmail.com>
2018-08-24 03:51:36 +0200

    cleaned up the extra spaces and comments

--- examples/hyperas_in_intermediate_fns.py
+++ examples/hyperas_in_intermediate_fns.py
@@ -9,12 +9,10 @@ from keras import backend as K
 from hyperopt import Trials, STATUS_OK, tpe
 from hyperas import optim
 from hyperas.distributions import choice, uniform
-from sklearn.metrics import roc_auc_score
 
 def euclidean_distance(vects):
     x, y = vects
     return K.sqrt(K.maximum(K.sum(K.square(x - y), axis=1, keepdims=True), K.epsilon()))
-
 
 def eucl_dist_output_shape(shapes):
     shape1, shape2 = shapes
@@ -36,9 +34,7 @@ def create_pairs(x, digit_indices):
             labels += [1, 0]
     return numpy.array(pairs), numpy.array(labels)
 
-
 def create_base_network(input_shape,dense_filter1,dense_filter2,dense_filter3,dropout1,dropout2):
-
     input = Input(shape=input_shape)
     x = Flatten()(input)
     x = Dense(dense_filter1, activation='relu')(x)
@@ -48,15 +44,11 @@ def create_base_network(input_shape,dense_filter1,dense_filter2,dense_filter3,dr
     x = Dense(dense_filter3, activation='relu')(x)
     return Model(input, x)
 
-
 def compute_accuracy(y_true, y_pred):
-
     pred = y_pred.ravel() < 0.5
     return numpy.mean(pred == y_true)
 
-
 def accuracy(y_true, y_pred):
-
     return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
 
 def process_data():
@@ -81,12 +73,9 @@ def data():
     return tr_pairs, tr_y, te_pairs, te_y,input_shape
 
 def contrastive_loss(y_true, y_pred):
-
     margin = 1
     return K.mean(y_true * K.square(y_pred) +
                   (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)))
-
-
 
 def create_model(tr_pairs, tr_y, te_pairs, te_y,input_shape):
     epochs = 20
@@ -101,9 +90,6 @@ def create_model(tr_pairs, tr_y, te_pairs, te_y,input_shape):
     input_a = Input(shape=input_shape)
     input_b = Input(shape=input_shape)
 
-    # because we re-use the same instance `base_network`,
-    # the weights of the network
-    # will be shared across the two branches
     processed_a = base_network(input_a)
     processed_b = base_network(input_b)
 
@@ -112,7 +98,6 @@ def create_model(tr_pairs, tr_y, te_pairs, te_y,input_shape):
 
     model = Model([input_a, input_b], distance)
 
-    # train
     rms = RMSprop()
     model.compile(loss=contrastive_loss, optimizer=rms, metrics=[accuracy])
     model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
@@ -121,7 +106,6 @@ def create_model(tr_pairs, tr_y, te_pairs, te_y,input_shape):
               verbose=1,
               validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y))
 
-    # compute final accuracy on training and test sets
     y_pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
     tr_acc = compute_accuracy(tr_y, y_pred)
     y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
@@ -144,4 +128,3 @@ if __name__ == '__main__':
     print("Evalutation of best performing model:")
     loss,te_acc = best_model.evaluate([te_pairs[:, 0], te_pairs[:, 1]], te_y)
     print("best prediction accuracy on test data %0.2f%%" % (100 * te_acc))
-
